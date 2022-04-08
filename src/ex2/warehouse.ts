ff97d3544268b6c236de77ac831c39972e1425fb
arpple <apple0239@gmail.com>
2022-04-08 20:52:24 +0700

    use set cache

--- src/ex2/warehouse.ts
+++ src/ex2/warehouse.ts
@@ -6,6 +6,7 @@ export namespace Warehouse {
 		isVentilated: boolean,
 		label: string,
 		cells: Cell.T[],
+		cellCaches: Set<string>,
 	}
 
 	export type Map = {
@@ -21,24 +22,28 @@ export namespace Warehouse {
 			.reduce((map, cell) => {
 				const connectedWarehouses = getConnectedWarehouse(map, cell)
 				if (connectedWarehouses.length === 0) {
+					const warehouse = createWarehouse(cellMap, cell)
+
 					return pipeWith(
 						map,
-						addWarehouse(createWarehouse(cellMap, cell)),
+						addWarehouse(warehouse),
 					)
 
 				} else if (connectedWarehouses.length === 1) {
 					const warehouse = connectedWarehouses[0]
 					warehouse.cells.push(cell)
-					warehouse.isVentilated = warehouse.isVentilated
-						|| isVentilated(cellMap, cell)
+					warehouse.cellCaches.add(createCellCache(cell))
+					warehouse.isVentilated = warehouse.isVentilated || isVentilated(cellMap, cell)
 
 					return map
 
 				} else {
+					const warehouse = mergeWarehouses(connectedWarehouses, cellMap, cell)
+
 					return pipeWith(
 						map,
 						removeWarehouses(connectedWarehouses),
-						addWarehouse(mergeWarehouses(connectedWarehouses, cellMap, cell)),
+						addWarehouse(warehouse),
 					)
 				}
 			}, newMap)
@@ -53,18 +58,22 @@ export namespace Warehouse {
 			cells: [cell],
 			isVentilated: isVentilated(cellMap, cell),
 			label: cell.label,
+			cellCaches: new Set([createCellCache(cell)]),
 		}
 	}
 
 	function mergeWarehouses(warehouses: T[], cellMap: Cell.Map, cell: Cell.T): T {
+		const cells = [
+			...warehouses.flatMap((w) => w.cells),
+			cell,
+		]
+
 		return {
 			isVentilated: warehouses.some((w) => w.isVentilated)
 				|| isVentilated(cellMap, cell),
 			label: cell.label,
-			cells: [
-				...warehouses.flatMap((w) => w.cells),
-				cell,
-			]
+			cells,
+			cellCaches: new Set(cells.map(createCellCache)),
 		}
 	}
 
@@ -85,13 +94,11 @@ export namespace Warehouse {
 	}
 
 	function getConnectedWarehouse(map: Map, cell: Cell.T): T[] {
-		const isCellConnected = (a: Cell.T, b: Cell.T): boolean => {
-			return Math.abs(a.x - b.x) + Math.abs(a.y - b.y) === 1
-		}
+		const left = createCellCache({ x: cell.x - 1, y: cell.y, label: cell.label })
+		const up = createCellCache({ x: cell.x, y: cell.y - 1, label: cell.label })
 
 		return map.warehouses
-			.filter((w) => w.label === cell.label)
-			.filter((w) => w.cells.find((c) => isCellConnected(c, cell)))
+			.filter((w) => w.cellCaches.has(left) || w.cellCaches.has(up))
 	}
 
 	function isVentilated(map: Cell.Map, cell: Cell.T): boolean {
@@ -106,5 +113,9 @@ export namespace Warehouse {
 			|| down === undefined
 
 		return isVentilated
+	}
+
+	function createCellCache(cell: Cell.T): string {
+		return `${cell.x},${cell.y},${cell.label}`
 	}
 }
